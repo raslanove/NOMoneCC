4d8f31d7ac8974135723b356cc747501f32544ce
Omar El Sayyed <omar@nomone.com>
2021-11-18 00:51:47 +0200

    - Implemented all comparison instructions. - Some refactoring.

--- Samples/Nand2TetrisVMTranslator/Src/VMTranslate.c
+++ Samples/Nand2TetrisVMTranslator/Src/VMTranslate.c
@@ -6,18 +6,43 @@
 
 struct OutputData {
     struct NString code;
+    int32_t lastLabelIndex;
 };
 
-void emitCode(struct NCC* ncc, const char* code) {
-    NString.append(&((struct OutputData*) ncc->extraData)->code, "%s", code);
+void emitCode(struct NCC* ncc, const char* format, ...) {
+    va_list vaList;
+    va_start(vaList, format);
+    NString.vAppend(&((struct OutputData*) ncc->extraData)->code, format, vaList);
+    va_end(vaList);
 }
 
 void emitInitializationCode(struct NCC* ncc) {
+    // // Initialization,
+    // (SP)
+    // @0
+    // (LCL)
+    // @1
+    // (ARG)
+    // @2
+    // (THIS)
+    // @3
+    // (THAT)
+    // @4
+    //
+    // // Set stack pointer,
+    // @256
+    // D=A
+    // @SP
+    // M=D
     emitCode(ncc, "// Initialization,\n(SP)\n@0\n(LCL)\n@1\n(ARG)\n@2\n(THIS)\n@3\n(THAT)\n@4\n\n");
-    //emitCode(ncc, "// Set stack pointer,\n@256\nD=A\n@SP\nM=D\n\n");
+    emitCode(ncc, "// Set stack pointer,\n@256\nD=A\n@SP\nM=D\n\n");
 }
 
 void emitTerminationCode(struct NCC* ncc) {
+    // // Termination,
+    // (TERMINATION)
+    // @TERMINATION
+    // 0;JMP
     emitCode(ncc, "// Termination,\n(TERMINATION)\n@TERMINATION\n0;JMP\n");
 }
 
@@ -62,11 +87,8 @@ void pushListener(struct NCC* ncc, struct NString* ruleName, int32_t variablesCo
         //   M=D
         //   @SP
         //   M=M+1
-        emitCode(ncc, "// push constant");
-        emitCode(ncc, NString.get(&valueVariable.value));
-        emitCode(ncc, "\n@");
-        emitCode(ncc, NString.get(&valueVariable.value));
-        emitCode(ncc, "\nD=A\n@SP\nA=M\nM=D\n@SP\nM=M+1\n\n");
+        const char* valueString = NString.get(&valueVariable.value);
+        emitCode(ncc, "// push constant %s\n@%s\nD=A\n@SP\nA=M\nM=D\n@SP\nM=M+1\n\n", valueString, valueString);
     }
     NCC_destroyVariable(&valueVariable);
     NCC_destroyVariable(&variable);
@@ -95,20 +117,97 @@ void addListener(struct NCC* ncc, struct NString* ruleName, int32_t variablesCou
     emitCode(ncc, "// add\n@SP\nM=M-1\n@SP\nA=M\nD=M\n@SP\nM=M-1\nA=M\nM=D+M\n@SP\nM=M+1\n\n");
 }
 
+void emitComparisonCode(struct NCC* ncc, const char* instruction, const char* jump) {
+
+    // Example:
+    //   eq
+    //
+    // Expecting 0 variables.
+
+    // Need two labels,
+    struct OutputData* outputData = (struct OutputData*) ncc->extraData;
+    int32_t labelIndex1 = outputData->lastLabelIndex + 1;
+    int32_t labelIndex2 = labelIndex1 + 1;
+    outputData->lastLabelIndex += 2;
+
+    // Code:
+    //   // eq
+    //   @SP
+    //   M=M-1
+    //
+    //   @SP
+    //   A=M
+    //   D=M
+    //
+    //   @SP
+    //   M=M-1
+    //   A=M
+    //   D=D-M
+    //
+    //   @Label1
+    //   D;JEQ     // Or any other compare operation.
+    //
+    //   @SP
+    //   A=M
+    //   M=0
+    //
+    //   @Label2
+    //   0;JMP
+    //
+    //   (Label1)
+    //   @SP
+    //   A=M
+    //   M=-1
+    //
+    //   (Label2)
+    //   @SP
+    //   M=M+1
+    emitCode(ncc, "// %s\n@SP\nM=M-1\n@SP\nA=M\nD=M\n@SP\nM=M-1\nA=M\nD=D-M\n@Label%d\nD;%s\n@SP\nA=M\nM=0\n@Label%d\n0;JMP\n(Label%d)\n@SP\nA=M\nM=-1\n(Label%d)\n@SP\nM=M+1\n\n", instruction, labelIndex1, jump, labelIndex2, labelIndex1, labelIndex2);
+}
+
+void eqListener(struct NCC* ncc, struct NString* ruleName, int32_t variablesCount) {
+    emitComparisonCode(ncc, "eq", "JEQ");
+}
+
+void ltListener(struct NCC* ncc, struct NString* ruleName, int32_t variablesCount) {
+    emitComparisonCode(ncc, "lt", "JGT");
+}
+
+void gtListener(struct NCC* ncc, struct NString* ruleName, int32_t variablesCount) {
+    emitComparisonCode(ncc, "gt", "JLT");
+}
+
 void NMain() {
 
     NSystemUtils.logI("sdf", "besm Allah :)");
 
-    const char* text = "// This file is part of www.nand2tetris.org\n"
-                       "// and the book \"The Elements of Computing Systems\"\n"
-                       "// by Nisan and Schocken, MIT Press.\n"
-                       "// File name: projects/07/StackArithmetic/SimpleAdd/SimpleAdd.vm\n"
-                       "\n"
-                       "// Pushes and adds two constants.\n"
-                       "push constant 7\n"
-                       "push constant 8\n"
-                       "add\n"
-                       "\"besm Allah \\\" :) \\ \" \n";
+    const char* text = "push constant 17\n"
+                       "push constant 17\n"
+                       "eq\n"
+                       "push constant 17\n"
+                       "push constant 16\n"
+                       "eq\n"
+                       "push constant 16\n"
+                       "push constant 17\n"
+                       "eq\n"
+                       "push constant 892\n"
+                       "push constant 891\n"
+                       "lt\n"
+                       "push constant 891\n"
+                       "push constant 892\n"
+                       "lt\n"
+                       "push constant 891\n"
+                       "push constant 891\n"
+                       "lt\n"
+                       "push constant 32767\n"
+                       "push constant 32766\n"
+                       "gt\n"
+                       "push constant 32766\n"
+                       "push constant 32767\n"
+                       "gt\n"
+                       "push constant 32766\n"
+                       "push constant 32766\n"
+                       "gt";
 
     // Create ncc,
     struct NCC ncc;
@@ -117,6 +216,7 @@ void NMain() {
     // Add data to accumulate output,
     struct OutputData outputData;
     NString.initialize(&outputData.code);
+    outputData.lastLabelIndex = 0;
     ncc.extraData = &outputData;
 
     // Elements,
@@ -129,7 +229,10 @@ void NMain() {
     NCC_addRule(&ncc, "StackModifier", "constant", 0, False);
     NCC_addRule(&ncc, "Push", "push ${WhiteSpace} ${StackModifier} ${WhiteSpace} ${Integer}", pushListener, False);
     NCC_addRule(&ncc, "Add", "add", addListener, False);
-    NCC_addRule(&ncc, "Instruction", "${Push} | ${Add}", 0, False);
+    NCC_addRule(&ncc, "Eq", "eq", eqListener, False);
+    NCC_addRule(&ncc, "LT", "lt", ltListener, False);
+    NCC_addRule(&ncc, "GT", "gt", gtListener, False);
+    NCC_addRule(&ncc, "Instruction", "${Push} | ${Add} | ${Eq} | ${LT} | ${GT}", 0, False);
 
     // Document,
     NCC_addRule(&ncc, "Document", "{${WhiteSpace} | ${LineComment} | ${Instruction}}^*", 0, True);
