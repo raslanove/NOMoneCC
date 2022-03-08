1feb00a04f611cdcaa85a005daf16c66c2fac985
Omar El Sayyed <omar@nomone.com>
2022-03-08 23:08:28 +0200

    - Parsed floating point constants.

--- Samples/CCodePretifierColorizer/Src/CCodePretifierColorizer.c
+++ Samples/CCodePretifierColorizer/Src/CCodePretifierColorizer.c
@@ -18,7 +18,11 @@ void NMain() {
     NSystemUtils.logI("C", "besm Allah :)\n");
 
     const char* code =
-            "0xfLLU";
+            "0x8.8p+2L";
+
+    //float a = .2e-2f;
+    //float a = 0x8.8p+2;
+    //NLOGW("sdf", "A is %f", a);
 
     // Substitute,
     struct NCC ncc;
@@ -28,7 +32,8 @@ void NMain() {
     NCC_addRule(&ncc, "ε", "", 0, False, False, False);
     NCC_addRule(&ncc, "digit", "0-9", 0, False, False, False);
     NCC_addRule(&ncc, "non-digit", "_|a-z|A-Z", 0, False, False, False);
-    NCC_addRule(&ncc, "hexadecimal-digit", "0-9|a-f", 0, False, False, False);
+    NCC_addRule(&ncc, "hexadecimal-prefix", "0x|X", 0, False, False, False);
+    NCC_addRule(&ncc, "hexadecimal-digit", "0-9|a-f|A-F", 0, False, False, False);
     NCC_addRule(&ncc, "hex-quad", "${hexadecimal-digit}${hexadecimal-digit}${hexadecimal-digit}${hexadecimal-digit}", 0, False, False, False);
     NCC_addRule(&ncc, "universal-character-name", "{\\\\u ${hex-quad}} | {\\\\U ${hex-quad} ${hex-quad}}", 0, False, False, False);
 
@@ -37,14 +42,34 @@ void NMain() {
     NCC_addRule(&ncc, "identifier", "${identifier-non-digit} {${digit} | ${identifier-non-digit}}^*", 0, False, True, False);
 
     // Constants,
+    // Integer constant,
     NCC_addRule(&ncc, "decimal-constant", "1-9 0-9^*", 0, False, False, False);
     NCC_addRule(&ncc, "octal-constant", "0 0-7^*", 0, False, False, False);
-    NCC_addRule(&ncc, "hexadecimal-constant", "{0x}|{0X} ${hexadecimal-digit} ${hexadecimal-digit}^*", 0, False, False, False);
+    NCC_addRule(&ncc, "hexadecimal-constant", "${hexadecimal-prefix} ${hexadecimal-digit} ${hexadecimal-digit}^*", 0, False, False, False);
     NCC_addRule(&ncc, "integer-suffix", "{ u|U l|L|{ll}|{LL}|${ε} } | { l|L|{ll}|{LL} u|U|${ε} }", 0, False, False, False);
     NCC_addRule(&ncc, "integer-constant", "${decimal-constant}|${octal-constant}|${hexadecimal-constant} ${integer-suffix}|${ε}", 0, False, True, False);
 
+    // Decimal floating point,
+    NCC_addRule(&ncc, "fractional-constant", "{0-9^* . 0-9 0-9^*} | {0-9 0-9^* . 0-9^*}", 0, False, False, False);
+    NCC_addRule(&ncc, "exponent-part", "e|E +|\\-|${ε} 0-9 0-9^*", 0, False, False, False);
+    NCC_addRule(&ncc, "floating-suffix", "f|l|F|L", 0, False, False, False);
+    NCC_addRule(&ncc, "decimal-floating-constant",
+            "{${fractional-constant} ${exponent-part}|${ε} ${floating-suffix}|${ε}} | "
+            "{0-9 0-9^* ${exponent-part} ${floating-suffix}|${ε}}", 0, False, False, False);
+
+    // Hexadecimal floating point,
+    NCC_addRule(&ncc, "hexadecimal-fractional-constant",
+            "{${hexadecimal-digit}^* . ${hexadecimal-digit} ${hexadecimal-digit}^*} | "
+            "{${hexadecimal-digit} ${hexadecimal-digit}^* . ${hexadecimal-digit}^*}", 0, False, False, False);
+    NCC_addRule(&ncc, "binary-exponent-part", "p|P +|\\-|${ε} 0-9 0-9^*", 0, False, False, False);
+    NCC_addRule(&ncc, "hexadecimal-floating-constant",
+                "${hexadecimal-prefix} ${hexadecimal-fractional-constant}|{${hexadecimal-digit}${hexadecimal-digit}^*} ${binary-exponent-part} ${floating-suffix}|${ε}", 0, False, False, False);
+
+    // Floating point constant,
+    NCC_addRule(&ncc, "floating-constant", "${decimal-floating-constant} | ${hexadecimal-floating-constant}", 0, False, True, False);
+
     // Document,
-    NCC_addRule(&ncc, "testDocument", "${identifier} | ${integer-constant}", printListener, True, False, False);
+    NCC_addRule(&ncc, "testDocument", "${identifier} | ${integer-constant} | ${floating-constant}", printListener, True, False, False);
 
     // Match and cleanup,
     int32_t matchLength = NCC_match(&ncc, code);
@@ -56,7 +81,7 @@ void NMain() {
     if (matchLength == codeLength) {
         NLOGI("C", "Success!");
     } else {
-        NLOGW("C", "Failed! MatchLength: %d", matchLength);
+        NLOGE("C", "Failed! MatchLength: %d", matchLength);
     }
 
     NLOGI("", "");
